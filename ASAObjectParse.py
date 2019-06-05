150667ccea3853f86b5f27a0c59786d46434258d
Kevin Tompkins <ktompkins@compunet.biz>
2019-06-05 09:49:40 -0600

    Added handling for descriptions and additional comments

--- ASAObjectParse.py
+++ ASAObjectParse.py
@@ -2,30 +2,30 @@ import ntpath
 import netmaskdictionary
 
 # Input the file in full directory format, IE: C:\temp\file.txt
-fileLocation = input("Location of ASA Object Configuration: ")
+fileLocation = input('Location of ASA Object Configuration: ')
 fileDir = ntpath.dirname(fileLocation)
 
 # Import Dictionary of netmasks in CIDR
 netmaskDict = netmaskdictionary.netmaskDict
 
 # Read the document into a List
-asaConfigFile = open(fileLocation, "r")
+asaConfigFile = open(fileLocation, 'r')
 row = [line.split(' ') for line in asaConfigFile.readlines()]
 asaConfigFile.close()
 
 # Pull out the first line to get the tag and address-group name, then remove it from the list
 group = row[0]
 del row[0]
-print(group)
 name = group[2].strip('\n')
 
-# Check for description and add if it exists
-if "description" in row[0][1]:
-    description = row[0][1]
+# Check for a description and add if it exists
+if 'description' in row[0][1]:
+    description = ' '.join(row[0][2:]).strip('\n')
     del row[0]
 else:
-    description = ""
+    description = ''
 
+# Create a list of addresses and remove the leading spaces in the original list
 objects = []
 for items in row:
     address = []
@@ -33,19 +33,24 @@ for items in row:
         address.append(items[i])
     objects.append(address)
 
+# Convert objects list into Palo Alto set command syntax
 setAddress = []
 for items in objects:
-    if items[1] == "host":
+    if items[1] == 'host':
         addr = items[2].strip('\n')
-        setAddress.append("set address IP-"+addr+" tag "+name+" ip-netmask "+addr+"\n")
+        setAddress.append('set address IP-'+addr+' tag '+name+' ip-netmask '+addr+'\n')
     else:
         addr = items[1]
         netmask = items[2].strip('\n')
         netmask = netmaskDict[netmask]
-        setAddress.append("set address IP-"+addr+" tag "+name+" ip-netmask "+addr+netmask+"\n")
+        setAddress.append('set address IP-'+addr+' tag '+name+' ip-netmask '+addr+netmask+'\n')
 
-with open(fileDir+"\\"+name+"-palo.txt", "w+") as file_handler:
-    file_handler.write("set tag "+name+"\n")
-    file_handler.write("set address-group "+name+" dynamic filter '"+name+"'\n")
+# Save set commands to output file
+with open(fileDir+'\\'+name+'-palo.txt', 'w+') as file_handler:
+    file_handler.write('set tag '+name+'\n')
+    if not description:
+        file_handler.write('set address-group '+name+' dynamic filter '+name+'\n')
+    else:
+        file_handler.write('set address-group '+name+' description "'+description+'" dynamic filter '+name+'\n')
     for item in setAddress:
-        file_handler.write("{}".format(item))
+        file_handler.write('{}'.format(item))
